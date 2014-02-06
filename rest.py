ef436351babcb1ade0a2dc1dabc0a77dc8e8759d
Alex Wright <alex@xeentech.com>
2014-02-06 00:10:15 +0000

    Adding a Tree resource endpoint.
    Will return a JSON of either a tree (only one level) or a blob.
    URI accepts branches and paths, branch names with a / in will break this for
    now.

--- rest.py
+++ rest.py
@@ -42,6 +42,25 @@ def traverse(repo, tree, path):
         return traverse(repo, obj, path)
     raise TreeTraversalError("tried to traverse a non-tree")
 
+def obj_to_json(repo, obj):
+    types = { 1: "commit", 2: "tree", 3: "blob" }
+    if type(obj) == pygit2.TreeEntry:
+        #return obj_to_json(repo, repo[obj.oid])
+        return {
+            "name": obj.name,
+            "type": types[repo[obj.oid].type],
+        }
+    if obj.type == pygit2.GIT_OBJ_TREE:
+        return {
+            "type": "tree",
+            "entries": map(lambda te: obj_to_json(repo, te), obj),
+        }
+    if obj.type == pygit2.GIT_OBJ_BLOB:
+        return {
+            "type": "blob",
+            "data": obj.data,
+        }
+
 class RepoDetail(restful.Resource):
     def get(self, repo_id):
         repo = get_repo(repo_id)
@@ -53,8 +72,23 @@ class RepoList(restful.Resource):
     def get(self):
         return repos.keys()
 
+class Tree(restful.Resource):
+    def get(self, repo_id, ref, path=None):
+        repo = get_repo(repo_id)
+        branch = repo.lookup_branch(ref)
+        commit = branch.get_object()
+        tree = commit.tree
+
+        path_parts = [] if path is None else path.split("/")
+        print "Parts: " + str(path_parts)
+        obj = traverse(repo, tree, path_parts)
+
+        return obj_to_json(repo, obj)
+
 api.add_resource(RepoList, "/repos")
 api.add_resource(RepoDetail, "/repos/<string:repo_id>")
+api.add_resource(Tree, "/tree/<string:repo_id>/<string:ref>",
+                       "/tree/<string:repo_id>/<string:ref>/<path:path>")
 
 if __name__ == '__main__':
     app.run(debug=True)
